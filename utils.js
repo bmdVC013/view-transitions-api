f74bbe616b4d846eb40bf06999f5a5a2ab0fd543
Dung Bui <bmdVC013@gmail.com>
2023-06-27 12:46:15 +0700

    :sparkles: Update expading image demo

--- utils.js
+++ utils.js
@@ -1,8 +1,9 @@
 export async function getPageContent(url) {
+  // This is a really scrappy way to do this.
   // Don't do this in production!
   const response = await fetch(url);
   const text = await response.text();
-
+  // Particularly as it uses regexp
   return /<body[^>]*>([\w\W]*)<\/body>/.exec(text)[1];
 }
 
@@ -10,46 +11,46 @@ function isBackNavigation(navigateEvent) {
   if (navigateEvent.navigationType === 'push' || navigateEvent.navigationType === 'replace') {
     return false;
   }
-
   if (
     navigateEvent.destination.index !== -1 &&
     navigateEvent.destination.index < navigation.currentEntry.index
   ) {
     return true;
   }
-
   return false;
 }
 
-// Intercept navigation
+// Intercept navigations
+// https://developer.chrome.com/docs/web-platform/navigation-api/
+// This is a naive usage of the navigation API, to keep things simple.
 export async function onLinkNavigate(callback) {
   navigation.addEventListener('navigate', (event) => {
     const toUrl = new URL(event.destination.url);
-
+    
     if (location.origin !== toUrl.origin) return;
-
+    
     const fromPath = location.pathname;
     const isBack = isBackNavigation(event);
-
+    
     event.intercept({
       async handler() {
         if (event.info === 'ignore') return;
-
+        
         await callback({
           toPath: toUrl.pathname,
           fromPath,
           isBack,
-        })
-      }
-    })
-  })
+        });
+      },
+    });
+  });
 }
 
 export function getLink(href) {
   const fullLink = new URL(href, location.href).href;
-
+  
   return [...document.querySelectorAll('a')].find((link) =>
-    link.href === fullLink  
+    link.href === fullLink
   );
 }
 
@@ -69,7 +70,7 @@ export function transitionHelper({
       domUpdated: updateCallbackDone,
       updateCallbackDone,
       finished: updateCallbackDone,
-    }
+    };
   }
 
   const classNamesArray = classNames.split(/\s+/g).filter(Boolean);
@@ -78,7 +79,7 @@ export function transitionHelper({
 
   const transition = document.startViewTransition(updateDOM);
 
-  transition.finished.finally(() => 
+  transition.finished.finally(() =>
     document.documentElement.classList.remove(...classNamesArray)
   );
 
